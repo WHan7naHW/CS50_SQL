b5f897ce1f1f8433824f1712a48fa5a3a4e05064
Han Wang <154707261+WHan7naHW@users.noreply.github.com>
2024-12-31 23:28:32 +0200

    Create private.sql

---
+++ P4/private/private.sql
@@ -0,0 +1,26 @@
+CREATE TEMPORARY TABLE "triplets" (
+    "sentence_id" INTEGER,
+    "start_position" INTEGER,
+    "length" INTEGER
+);
+
+
+INSERT INTO "triplets" ("sentence_id", "start_position", "length")
+VALUES
+    (14, 98, 4),
+    (114, 3, 5),
+    (618, 72, 9),
+    (630, 7, 3),
+    (932, 12, 5),
+    (2230, 50, 7),
+    (2346, 44, 10),
+    (3041, 14, 5);
+
+
+CREATE VIEW "message" AS
+SELECT
+    substr("sentences"."sentence", "triplets"."start_position", "triplets"."length") AS "phrase"
+FROM
+    "triplets"
+JOIN
+    "sentences" ON "triplets"."sentence_id" = "sentences"."id";
